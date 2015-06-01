2df7195555e99e18b438d9dc15c7391dddf4c971
Mark McQuillen <markdmcq@hotmail.com>
2015-05-31 22:09:17 -0700

    Showing two different colors

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,10 +2,22 @@ get '/' do
   erb :index
 end
 
-get '/things' do
+get '/sf' do
   if request.xhr?
     # resp = Unirest.get "https://api.forecast.io/forecast/81c88efeafdeb098caa777f0281510a8/37.8267,-122.423"
     resp = Unirest.get "https://dbc-feels-like.herokuapp.com/city/1/feels_like_yearly"
+    # resp = Unirest.get "https://dbc-feels-like.herokuapp.com/city/1/feels_like"
+    resp.body.to_json
+  else
+    status 404
+  end
+end
+
+
+get '/rich' do
+  if request.xhr?
+    # resp = Unirest.get "https://api.forecast.io/forecast/81c88efeafdeb098caa777f0281510a8/37.8267,-122.423"
+    resp = Unirest.get "https://dbc-feels-like.herokuapp.com/city/2/feels_like_yearly"
     # resp = Unirest.get "https://dbc-feels-like.herokuapp.com/city/1/feels_like"
     resp.body.to_json
   else
--- public/js/application.js
+++ public/js/application.js
@@ -65,6 +65,84 @@ var lineChart = (function(){
     .interpolate('linear');
     vis.append("svg:path")
       .attr("d", humidityLine(fkData))
+      .attr("stroke", "blue")
+      .attr("stroke-width", 2)
+      .attr("fill", "none")
+      .on('mouseover',function(d){
+        d3.select(this)
+          .attr('stroke-width',5);
+      })
+      .on('mouseout',function(d){
+        d3.select(this)
+          .attr('stroke-width',2);
+      });
+    };
+  var initChart2 = function(fkData){
+    var vis = d3.select("#visualisation"),
+      WIDTH = parseInt(d3.select('#things').style('width'),10),
+      HEIGHT = 500,
+      MARGINS = {
+        top: 20,
+        right: 20,
+        bottom: 20,
+        left: 50
+      },
+    xRange = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([d3.min(fkData, function (d) {
+          return d.date;
+        }),
+        d3.max(fkData, function (d) {
+          return d.date;
+        })
+      ]),
+    yRange = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([d3.min(fkData, function (d) {
+        return 0;
+      }),
+      d3.max(fkData, function (d) {
+        return d.high_apparent_temp;
+      })
+    ]),
+    xAxis = d3.svg.axis()
+      .scale(xRange)
+      .tickSize(5)
+      .tickSubdivide(true),
+    yAxis = d3.svg.axis()
+      .scale(yRange)
+      .tickSize(5)
+      .orient("left")
+      .tickSubdivide(true);
+
+    vis.append("svg:g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")")
+      .call(xAxis);
+    vis.append("svg:g")
+      .attr("class", "y axis")
+      .attr("transform", "translate(" + (MARGINS.left) + ",0)")
+      .call(yAxis);
+    var cloudCovLine = d3.svg.line()
+    .x(function (d) {
+      return xRange(d.date);
+    })
+    .y(function (d) {
+      return yRange(d.high_apparent_temp);
+    })
+    .interpolate('linear');
+    vis.append("svg:path")
+      .attr("d", cloudCovLine(fkData))
+      .attr("stroke", "red")
+      .attr("stroke-width", 2)
+      .attr("fill", "none");
+
+    var humidityLine = d3.svg.line()
+    .x(function (d) {
+      return xRange(d.date);
+    })
+    .y(function (d) {
+      return yRange(d.high_apparent_temp);
+    })
+    .interpolate('linear');
+    vis.append("svg:path")
+      .attr("d", humidityLine(fkData))
       .attr("stroke", "red")
       .attr("stroke-width", 2)
       .attr("fill", "none")
@@ -78,7 +156,8 @@ var lineChart = (function(){
       });
     };
   return {
-    initChart: initChart
+    initChart: initChart,
+    initChart2: initChart2,
   }
 
 })();
@@ -98,7 +177,7 @@ var size = (function(){
 $(document).ready(function() {
   $('body').on('click','#compare',function(event){
     event.preventDefault();
-    $.get("/things").done(function(data){
+    $.get("/sf").done(function(data){
       var fk_resp = JSON.parse(data).weather["2011"];
 
 
@@ -107,9 +186,9 @@ $(document).ready(function() {
       lineChart.initChart(fk_resp);
     });
 
-    $.get("/things").done(function(data){
+    $.get("/rich").done(function(data){
       var fk_resp = JSON.parse(data).weather["2012"];
-      lineChart.initChart(fk_resp);
+      lineChart.initChart2(fk_resp);
     });
   })
 });
