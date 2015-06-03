651960ad9339eac99aefbc758d51d7dfb04c6c28
Dudenator <markdmcq@gmail.com>
2015-06-03 10:57:24 -0700

    grab monthly data from api and average it. Yay

--- public/js/application.js
+++ public/js/application.js
@@ -5,7 +5,34 @@ $(function(){
     event.preventDefault()
 
     var selected_month = $(".month_form option:selected").val()
-    debugger
+
+    var resp_1 = $.get(
+      "https://dbc-feels-like.herokuapp.com/city/1/weather").done(function(reply){
+
+        var month_prop_totals = [0,0,0,0,0,0,0,0,0,0,0,0]
+        var month_days_count = [0,0,0,0,0,0,0,0,0,0,0,0]
+
+        var month_array = reply.data
+
+        for(var i = 0; i < month_array.length; i++){
+          month_prop_totals[month_array[i]["month"] - 1] += month_array[i]["high_temp"]
+          month_days_count[month_array[i]["month"] - 1] += 1
+
+        };
+
+        console.log(month_prop_totals)
+        console.log(month_days_count)
+
+        var month_prop_avgs = [];
+
+        for(var i = 0; i < month_prop_totals.length; i++){
+          var month = month_prop_totals[i]
+          month_prop_avgs.push(Math.round(month/month_days_count[i]))
+        };
+
+        console.log(month_prop_avgs)
+
+      });
 
   });
 
