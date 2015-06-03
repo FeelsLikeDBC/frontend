829ba482675b292a1965572a1f002d7a8b3cc9be
Dudenator <markdmcq@gmail.com>
2015-06-03 10:07:10 -0700

    add form to feels like page with listener that gets month selected

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,28 @@
 $(function(){
   cities.setEventListeners();
+
+  $(".month_form").on('submit', function(event){
+    event.preventDefault()
+
+    var selected_month = $(".month_form option:selected").val()
+    debugger
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
