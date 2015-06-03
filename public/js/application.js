342dadde87179de46504d9fdbdc1e9f9733ddb01
jackzampolin <jack.zampolin@gmail.com>
2015-06-03 14:32:23 -0700

    fixes to charts

--- public/js/application.js
+++ public/js/application.js
@@ -14,32 +14,43 @@
 
 
 // SINGLE CITY ARGS HASHES THAT WORK
-// var feelsLikeCity = {
-//   params: ['avg_apparent_day_temp','avg_apparent_night_temp','high_apparent_temp','low_apparent_temp'],
-//   colors: ['yellow','grey','red','blue'],
-//   names: ['Daytime','Nighttime','High','Low'],
-// }
-// var actualCity = {
-//   params: ['avg_temp','high_temp','low_temp'],
-//   colors: ['black','red','blue'],
-//   names: ['Average','High','Low'],
-// }
+var feelsLikeCity = {
+  params: ['avg_apparent_day_temp','avg_apparent_night_temp','high_apparent_temp','low_apparent_temp'],
+  colors: ['yellow','grey','red','blue'],
+  names: ['Daytime','Nighttime','High','Low'],
+}
 
-// TODO DAYTIME AND NIGHTTIME CHART
+var actualCity = {
+  params: ['avg_temp','high_temp','low_temp'],
+  colors: ['black','red','blue'],
+  names: ['Average','High','Low'],
+}
+
+// DAYNIGHT ARGS HASH
+var dayNight = {
+  params: ['sunrise','sunset'],
+  colors: ['yellow','black'],
+  names: ['Sunrise', 'Sunset'],
+}
+
 // TODO TOOLTIP UPGRADE
 // TODO LEGEND
+// Stretch is precipitation block chart
 
 $(function(){
   cities.setEventListeners();
   $('#go').on('click',function(){
     var from = cities.retrieve('from');
-    var to = cites.retrieve('to');
+    var to = cities.retrieve('to');
 
     // Shows the set piece single city charts
-    // lineCharts.singleCity(from or to,'#target',argsHash)
+    // lineCharts.singleCity(from,'#target',argsHash)
 
     // Shows the comparison between two cities for any of the params listed above
     // lineCharts.compareCities(from,to,'#target',param)
+
+    // Shows the time of Sunrise and Sunset for a single city
+    // lineCharts.dayNight(from, '#target', dayNight)
 
   });
 })
